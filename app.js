2f27e11af8f413d9b8540228bea1eff0c3f5cdc9
YourOwen <minyuwen@126.com>
2018-12-14 11:01:05 +0800

    program init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const ejs = require("ejs")
+
+app.set('view engine', 'ejs')
+
+app.get("/",(req,res)=>{
+    res.render("index",{})
+})
+
+app.listen(80,()=>{  //默认80端口 Url栏不显示
+    console.log("Running at http://127.0.0.1")
+})
