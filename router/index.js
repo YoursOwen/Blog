e9fd0076f8b8f13e380d9a99049a2dfb35f2d24a
YourOwen <minyuwen@126.com>
2018-12-15 23:06:37 +0800

    抽离封装

---
+++ router/index.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const router = express.Router()
+const ctrl = require('../controller')
+
+//主页
+router.get("/",ctrl.index)
+
+//注册页路由
+router.get("/register",ctrl.register)
+router.post("/register",ctrl.getRegister)
+
+//登录页
+router.get("/login",ctrl.login)
+router.post("/login",ctrl.makeLogin)
+
+module.exports = router
+
