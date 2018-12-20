239af78671c4b87d88e7a030319657f6eeb746b7
YourOwen <minyuwen@126.com>
2018-12-20 20:26:37 +0800

    主页分页栏

--- controller/index.js
+++ controller/index.js
@@ -3,22 +3,31 @@ const moment = require('moment');
 
 module.exports = {
     getIndex (req,res) {
-        const sqlStr = `select a.id,a.title,a.ctime,u.nickname from article 
-        as a LEFT JOIN 
+        let maxMesssage= 4
+        let currentPage = 1
+
+        const sqlStr = `select a.id,a.title,a.ctime,u.nickname from article
+        as a LEFT JOIN
         user as u on a.authorId = u.id
         ORDER BY a.ctime DESC
-        LIMIT 6` 
+        LIMIT ${maxMesssage};
+        select count(*) as count from article`
         conn.query(sqlStr,(err,results)=>{
-          
+
             if(err) return res.status("500").send({status:500,msg:"数据加载失败"+err.message})
+
+            let totalPage = Math.floor(results[1][0].count/maxMesssage)
+
             res.render("index",{
                 userInfo:req.session.userInfo,
                 isLogin:req.session.isLogin,
-                item:results
-            })  
+                item:results[0],
+                totalPage,
+                currentPage
+            })
         })
-        
-       
+
+
     },
     getLogout (req,res) {
         req.session.destroy(function(err) {
